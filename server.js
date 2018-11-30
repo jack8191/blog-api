c8109676c379c365c56d71647cbedddcc0a00707
jack eichner <jack8191@gmail.com>
2018-11-29 21:01:39 -0800

    Added content to files

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const { BlogPosts } = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+//a sample post
+BlogPosts.create('Thumbs','Thumbs go on the hands.', 'Thumbs Flannagan Mcgillicuddy', '1995');
+
+app.get('/blog-posts', (req, res) => {
+    res.json(BlogPosts.get());
+  });
+
+app.delete('/blog-posts/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted shopping list item \`${req.params.ID}\``);
+    res.status(204).end();
+  });
+
+app.post('/blog-posts', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(item);
+  });
+
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author', 'id'];
+    for (let i=0; i<requiredFields.length; i++) {
+      const field = requiredFields[i];
+      if (!(field in req.body)) {
+        const message = `Missing \`${field}\` in request body`
+        console.error(message);
+        return res.status(400).send(message);
+      }
+    }
+    if (req.params.id !== req.body.id) {
+      const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+    console.log(`Updating post \`${req.params.id}\``);
+    BlogPosts.update({
+      id: req.params.id,
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      publishDate: req.body.publishDate
+    });
+    res.status(204).end();
+  });
+
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+  });
